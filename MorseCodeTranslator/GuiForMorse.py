b866caae3932d824db864e8b699c317e458d4412
Mounika Durga Ampapurapu <70141179+amd9301@users.noreply.github.com>
2020-10-06 23:28:35 +0530

    Update GuiForMorse.py
    Updated font sizes and window size

--- MorseCodeTranslator/GuiForMorse.py
+++ MorseCodeTranslator/GuiForMorse.py
@@ -3,7 +3,7 @@ import MorseTranslator
 
 root = tk.Tk()
 
-root.geometry("500x500")
+root.geometry("600x300")
 # string variables for storing
 msg_var = tk.StringVar()
 morse_var = tk.StringVar()
@@ -13,31 +13,33 @@ def Encrypt():
     msg = msg_entry.get()
     msg.upper()
     cipher = MorseTranslator.encrypt(msg)
-    #print(cipher)
-    msg_ans.config(text=cipher)
+    # print(cipher)
+    msg_ans.config(text=cipher,font=('calibre', 20, 'bold'))
+
+
 def Decrypt():
     morse = morse_entry.get()
-    dec =MorseTranslator.decrypt(morse)
-    morse_ans.config(text=dec)
-    #print(dec)
+    dec = MorseTranslator.decrypt(morse)
+    morse_ans.config(text=dec,font=('calibre', 15, 'bold'))
+    # print(dec)
 
-#Head label
-head=tk.Label(root,text="  Note:Use Capital letters for ease of use   ")
+
+# Head label
+head = tk.Label(root, text="Note:Use Capital letters for ease of use  ", font=('calibre', 10, 'bold'))
 # Message labels
-msg_label = tk.Label(root, text="Message", font=('calibre', 10, 'bold'))
-msg_ans = tk.Label(root, text="Here is the Morse Code for your Message", font=('calibre', 10, 'bold'))
+msg_label = tk.Label(root, text="Message", font=('calibre', 15, 'bold'))
+msg_ans = tk.Label(root, text="Here is the Morse Code for your Message", font=('calibre', 13, 'bold'))
 
 # Message input entry
-msg_entry = tk.Entry(root, textvariable=msg_var, font=('calibre', 10, 'bold'))
+msg_entry = tk.Entry(root, textvariable=msg_var, font=('calibre', 15, 'bold'))
 
 # morse labels
 
-morse_label = tk.Label(root, text='Morse Code', font=('calibre', 10, 'bold'))
-morse_ans = tk.Label(root, text='Here is the Text for your MorseCode', font=('calibre', 10, 'bold'))
-
+morse_label = tk.Label(root, text='Morse Code', font=('calibre', 15, 'bold'))
+morse_ans = tk.Label(root, text='Here is the Text for your MorseCode', font=('calibre', 13, 'bold'))
 
 # creating entry for morse
-morse_entry = tk.Entry(root, textvariable=morse_var, font=('calibre', 10, 'normal'))
+morse_entry = tk.Entry(root, textvariable=morse_var, font=('calibre', 15, 'normal'))
 
 # Encrypt button
 enc_btn = tk.Button(root, text='Encrypt', command=Encrypt)
@@ -47,16 +49,14 @@ dec_btn = tk.Button(root, text='Decrypt', command=Decrypt)
 
 # placing the label and entry in
 # the required position using grid  method
-head.grid(row=2,column=3)
-msg_label.grid(row=3,column=2)
-msg_entry.grid(row=3,column=3)
-msg_ans.grid(row=4,column=3)
-enc_btn.grid(row=5,column=3)
-morse_label.grid(row=6,column=2)
-morse_entry.grid(row=6,column=3)
-morse_ans.grid(row=7,column=3)
+head.grid(row=2, column=3)
+msg_label.grid(row=3, column=2)
+msg_entry.grid(row=3, column=3)
+msg_ans.grid(row=4, column=3)
+enc_btn.grid(row=5, column=3)
+morse_label.grid(row=6, column=2)
+morse_entry.grid(row=6, column=3)
+morse_ans.grid(row=7, column=3)
+dec_btn.grid(row=8, column=3)
 
-dec_btn.grid(row=8,column=3)
-# performing an infinite loop
-# for the window to display
 root.mainloop()
